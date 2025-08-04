b89b78f20ce16b77e28649660c71149e7a5a3c61
TheJmqn <jman4461@gmail.com>
2025-08-04 00:00:39 -0400

    Style and homepage update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import photo from "./../assets/file.jpg";
-import cat from "./../assets/cat.jpg";
 
 export default function Home() {
   return (
@@ -14,24 +13,29 @@ export default function Home() {
         <h1>Welcome to Joe Moran Online</h1>
         <h2>Notice: This page is currently under construction.</h2>
         <p>
-            Welcome to the Joe Moran homepage. My name is Joe Moran (shocker), and I am a Computer Science major at the University of Virginia in Charlottesville.
-            I have worked as a software developer for the past three years at various government contractors. This page exists for both
-            professional and personal purposes, which you can find more about by exploring around.
+            Welcome to the Joe Moran homepage. My name, unsurprisingly, is Joe Moran, and I am a third-year Computer Science major at the University of Virginia in Charlottesville.
+            This website exists for both professional and personal purposes, which you can learn more about by exploring around.
         </p>
         <p>
-            I am making this page to create a more free form website where I can express my thoughts and also show off some
-            past projects. I think most social media platforms are a bit too restrictive in terms of the customization they allow,
-            so I just decided to quickly put together my own page instead. I'll still be posting links to this page on some social media though.
+          Professionally, I have worked as a software developer for the past three years at various government contractors. I have a passion for software development,
+          system design, and especially network and Internet architecture. To see some of my past projects, 
+          please review my resume or the <Link to="/work">Past Work</Link> page.
         </p>
-        <div className="home-buttons">
-          <div className="button-group">
-            <Link to="/notes" className="home-button notes-button" aria-label="Read My Notes">All Notes</Link>
-            <Link to="/notes/latest" className="home-button latest-note-button" aria-label="Latest Note">Latest Note</Link>
-            <Link to="/work" className="home-button projects-button" aria-label="See My Projects">Professional</Link>
-            <Link to="/misc" className="home-button hobbies-button" aria-label="Miscellaneous Topics">Misc</Link>
-            <Link to="/contact" className="home-button contact-button" aria-label="Contact Me">Contact</Link>
-          </div>
-        </div>
+        <p>
+            On the personal side of things, I have interests in topics such as sports, politics, music, design, and much more. 
+            I make biweekly <Link to="/notes">"Notes"</Link> articles about whatever topics come to mind in my life.
+            I am also working on the <Link to="/misc">Misc</Link> page to describe my interests and hobbies more in-depth. For now, you can check out
+            my <Link to="/contact">Social Media</Link> pages for more information. Thank you for visiting Joe Moran Online.
+        </p>
+      </div>
+    </div>
+    <div className="home-buttons">
+      <div className="button-group">
+        <Link to="/notes" className="home-button notes-button" aria-label="Read My Notes">All Notes</Link>
+        <Link to="/notes/latest" className="home-button latest-note-button" aria-label="Latest Note">Latest Note</Link>
+        <Link to="/work" className="home-button projects-button" aria-label="See My Projects">Professional</Link>
+        <Link to="/misc" className="home-button hobbies-button" aria-label="Miscellaneous Topics">Misc</Link>
+        <Link to="/contact" className="home-button contact-button" aria-label="Contact Me">Contact</Link>
       </div>
     </div>
   </div>
